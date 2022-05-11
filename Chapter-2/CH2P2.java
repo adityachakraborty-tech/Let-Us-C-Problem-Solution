21977bc51d4ffbecc5870eaa4d69bc5dd8556ac2
Aditya Chakraborty <75158043+adityachakraborty-tech@users.noreply.github.com>
2022-05-11 13:58:00 +0530

    Update CH2P2.java

--- Chapter-2/CH2P2.java
+++ Chapter-2/CH2P2.java
@@ -9,6 +9,7 @@ public class CH2P2 {
         while (n > 0) {
             d = n % 10;
             rev = rev * 10 + d;
+            n/=10;
 
         }
         System.out.println("Reverse is :  " + rev);
